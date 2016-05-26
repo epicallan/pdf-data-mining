b96859c5ffd38290b423ec3045ea3d020dd023ae
allan <epicallan.al@gmail.com>
2016-05-26 09:49:52 +0300

    manual method working

--- src/app.js
+++ src/app.js
@@ -3,14 +3,24 @@ import readline from 'readline';
 import verEx from 'verbal-expressions';
 import csv from 'fast-csv';
 
+const rootPath = '/Users/allanlukwago/apps/budget-data/samples';
+
 const readFileByLine = readline.createInterface({
-  input: fs.createReadStream('/Users/allanlukwago/apps/budget-data/samples/2014-15.txt')
+  input: fs.createReadStream(`${rootPath}/2014-15.txt`)
 });
 
+const tableSegmentToRead = {
+  startLine: 'GoU Releases and Expenditure by Output*',
+  endLine: 'QUARTER 4: Highlights of Vote Performance'
+};
+
+const tableName = tableSegmentToRead.startLine;
 // configuring csv writableStream
 const csvStream = csv.createWriteStream({ headers: true });
-const writableStream = fs.createWriteStream('my.csv');
 
+const writableStream = fs.createWriteStream(`${rootPath}/${tableName}.csv`);
+
+// adding a transformation function that is responsible for the row title
 csvStream.transform(row => ({
   sector: row[0],
   'Approved Budget': row[1],
@@ -22,32 +32,44 @@ csvStream.transform(row => ({
 }))
 .pipe(writableStream);
 
+const csvLineTowrite = (line) => line.split('  ').filter(word => word.length > 1);
 
-const fileLines = [];
-let isTable = false;
+const writeLineToFile = (line) => {
+  const csvLine = csvLineTowrite(line);
+  if (csvLine.length === 7) csvStream.write(csvLine);
+  return csvLine;
+};
 
-readFileByLine.on('line', (line) => {
-  const startMining = verEx().find('Releases and Expenditure by Vote Function');
-  const endMining = verEx().find('Excluding Taxes and Arrears');
-  const isEnd = endMining.test(line);
-  if (isTable) {
-    if (line.length > 1 && !isEnd) fileLines.push(line);
-    if (isEnd) {
-      isTable = false;
-      readFileByLine.close();
-    }
+/* eslint-disable consistent-return */
+
+const isEndOfTable = (endPhrase, line) => {
+  if (endPhrase) {
+    const endMiningExpression = verEx().find(endPhrase);
+    return endMiningExpression.test(line);
   }
-  if (startMining.test(line)) isTable = true;
-});
+  const csvLine = csvLineTowrite(line);
+  if (csvLine.length < 3) return true;
+};
+
+const readInFile = (segment) => {
+  let isTable = false;
+  readFileByLine.on('line', (line) => {
+    const startMining = verEx().find(segment.startLine);
+    const isEnd = isEndOfTable(segment.endLine, line);
+    if (isTable) {
+      if (line.length > 1 && !isEnd) writeLineToFile(line);
+      if (isEnd) {
+        isTable = false;
+        console.log('we could be done');
+      }
+    }
+    if (startMining.test(line)) isTable = true; // write in new Table
+  });
+};
+
 
 readFileByLine.on('close', () => {
-  // console.log('first line', fileLines[0]);
-  const splicedFileLines = fileLines.splice(3, fileLines.length);
-  // console.log(splicedFileLines);
-  splicedFileLines.forEach(line => {
-    const csvLine = line.split('  ').filter(word => word.length > 1);
-    // console.log('line - ', csvLine);
-    csvStream.write(csvLine);
-  });
-  // csvStream.end();
+  csvStream.end();
 });
+
+readInFile(tableSegmentToRead);
---
+++ src/config.js
@@ -0,0 +1,4 @@
+export const sectionsToMine = [
+  { startLine: 'GoU Releases and Expenditure by Output*', endLine: null },
+  { startLine: 'Highlights of Vote Performance', endLine: '* Excluding Taxes and Arrears' }
+];
