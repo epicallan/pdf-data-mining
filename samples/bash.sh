9e213e72ede3593f05b699092e4838fb7d7ab3bc
allan <epicallan.al@gmail.com>
2016-05-27 18:31:08 +0300

    rebase issue

---
+++ samples/bash.sh
@@ -0,0 +1,184 @@
+budget -f NaN -l 137 -n AnnualBudgetPerformanceReport 2014-15.pdf 
+budget -o -f NaN -l NaN -n AnnualBudgetPerformanceReport-2 2014-15.pdf 
+budget -f 138 -l 171 -n MinistryofAgricultureAnimalIndustryFisheries 2014-15.pdf 
+budget -o -f 138 -l 139 -n MinistryofAgricultureAnimalIndustryFisheries-2 2014-15.pdf 
+budget -f 172 -l 175 -n DairyDevelopmentAuthority 2014-15.pdf 
+budget -o -f 172 -l 173 -n DairyDevelopmentAuthority-2 2014-15.pdf 
+budget -f 176 -l 179 -n KampalaCityCouncilAuthority 2014-15.pdf 
+budget -o -f 176 -l 177 -n KampalaCityCouncilAuthority-2 2014-15.pdf 
+budget -f 180 -l 185 -n NationalAnimalGeneticResourceCentreandDataBank 2014-15.pdf 
+budget -o -f 180 -l 181 -n NationalAnimalGeneticResourceCentreandDataBank-2 2014-15.pdf 
+budget -f 186 -l 191 -n NationalAgriculturalResearchOrganisation 2014-15.pdf 
+budget -o -f 186 -l 187 -n NationalAgriculturalResearchOrganisation-2 2014-15.pdf 
+budget -f 192 -l 201 -n NAADSSecretariat 2014-15.pdf 
+budget -o -f 192 -l 193 -n NAADSSecretariat-2 2014-15.pdf 
+budget -f 202 -l 206 -n UgandaCottonDevelopmentOrganisation 2014-15.pdf 
+budget -o -f 202 -l 203 -n UgandaCottonDevelopmentOrganisation-2 2014-15.pdf 
+budget -f 207 -l 213 -n UgandaCoffeeDevelopmentAuthority 2014-15.pdf 
+budget -o -f 207 -l 208 -n UgandaCoffeeDevelopmentAuthority-2 2014-15.pdf 
+budget -f 214 -l 222 -n MinistryofLandsHousingUrbanDevelopment 2014-15.pdf 
+budget -o -f 214 -l 215 -n MinistryofLandsHousingUrbanDevelopment-2 2014-15.pdf 
+budget -f 223 -l 224 -n KampalaCityCouncilAuthority 2014-15.pdf 
+budget -o -f 223 -l 224 -n KampalaCityCouncilAuthority-2 2014-15.pdf 
+budget -f 225 -l 228 -n UgandaLandCommission 2014-15.pdf 
+budget -o -f 225 -l 226 -n UgandaLandCommission-2 2014-15.pdf 
+budget -f 229 -l 243 -n MinistryofEnergyandMineralDevelopment 2014-15.pdf 
+budget -o -f 229 -l 230 -n MinistryofEnergyandMineralDevelopment-2 2014-15.pdf 
+budget -f 244 -l 246 -n RuralElectrificationAgency 2014-15.pdf 
+budget -o -f 244 -l 245 -n RuralElectrificationAgency-2 2014-15.pdf 
+budget -f 247 -l 262 -n MinistryofWorksandTransport 2014-15.pdf 
+budget -o -f 247 -l 248 -n MinistryofWorksandTransport-2 2014-15.pdf 
+budget -f 263 -l 268 -n UgandaNationalRoadAuthority 2014-15.pdf 
+budget -o -f 263 -l 264 -n UgandaNationalRoadAuthority-2 2014-15.pdf 
+budget -f 269 -l 273 -n UgandaRoadFund 2014-15.pdf 
+budget -o -f 269 -l 270 -n UgandaRoadFund-2 2014-15.pdf 
+budget -f 274 -l 278 -n KampalaCityCouncilAuthority 2014-15.pdf 
+budget -o -f 274 -l 275 -n KampalaCityCouncilAuthority-2 2014-15.pdf 
+budget -f 279 -l 285 -n MinistryofInformationCommunicationsTechnology 2014-15.pdf 
+budget -o -f 279 -l 280 -n MinistryofInformationCommunicationsTechnology-2 2014-15.pdf 
+budget -f 286 -l 290 -n NationalInformationTechnologyAuthority 2014-15.pdf 
+budget -o -f 286 -l 287 -n NationalInformationTechnologyAuthority-2 2014-15.pdf 
+budget -f 291 -l 339 -n MinistryofTradeIndustryandCooperatives 2014-15.pdf 
+budget -o -f 291 -l 292 -n MinistryofTradeIndustryandCooperatives-2 2014-15.pdf 
+budget -f 340 -l 346 -n MinistryofTourismWildlifeandAntiquities 2014-15.pdf 
+budget -o -f 340 -l 341 -n MinistryofTourismWildlifeandAntiquities-2 2014-15.pdf 
+budget -f 347 -l 354 -n UgandaIndustrialResearchInstitute 2014-15.pdf 
+budget -o -f 347 -l 348 -n UgandaIndustrialResearchInstitute-2 2014-15.pdf 
+budget -f 355 -l 358 -n UgandaTourismBoard 2014-15.pdf 
+budget -o -f 355 -l 356 -n UgandaTourismBoard-2 2014-15.pdf 
+budget -f 359 -l 362 -n UgandaNationalBureauofStandards 2014-15.pdf 
+budget -o -f 359 -l 360 -n UgandaNationalBureauofStandards-2 2014-15.pdf 
+budget -f 363 -l 387 -n MinistryofEducationandSports 2014-15.pdf 
+budget -o -f 363 -l 364 -n MinistryofEducationandSports-2 2014-15.pdf 
+budget -f 388 -l 393 -n BusitemaUniversity 2014-15.pdf 
+budget -o -f 388 -l 389 -n BusitemaUniversity-2 2014-15.pdf 
+budget -f 394 -l 399 -n KampalaCityCouncilAuthority 2014-15.pdf 
+budget -o -f 394 -l 395 -n KampalaCityCouncilAuthority-2 2014-15.pdf 
+budget -f 400 -l 403 -n MuniUniversity 2014-15.pdf 
+budget -o -f 400 -l 401 -n MuniUniversity-2 2014-15.pdf 
+budget -f 404 -l NaN -n EducationServiceCommission 2014-15.pdf 
+budget -o -f 404 -l 405 -n EducationServiceCommission-2 2014-15.pdf 
+budget -f NaN -l 407 -n AnnualBudgetPerformanceReport 2014-15.pdf 
+budget -o -f NaN -l NaN -n AnnualBudgetPerformanceReport-2 2014-15.pdf 
+budget -f 408 -l 412 -n MakerereUniversity 2014-15.pdf 
+budget -o -f 408 -l 409 -n MakerereUniversity-2 2014-15.pdf 
+budget -f 413 -l 417 -n MbararaUniversity 2014-15.pdf 
+budget -o -f 413 -l 414 -n MbararaUniversity-2 2014-15.pdf 
+budget -f 418 -l 421 -n MakerereUniversityBusinessSchool 2014-15.pdf 
+budget -o -f 418 -l 419 -n MakerereUniversityBusinessSchool-2 2014-15.pdf 
+budget -f 422 -l 425 -n KyambogoUniversity 2014-15.pdf 
+budget -o -f 422 -l 423 -n KyambogoUniversity-2 2014-15.pdf 
+budget -f 426 -l 428 -n UgandaManagementInstitute 2014-15.pdf 
+budget -o -f 426 -l 427 -n UgandaManagementInstitute-2 2014-15.pdf 
+budget -f 429 -l 436 -n GuluUniversity 2014-15.pdf 
+budget -o -f 429 -l 430 -n GuluUniversity-2 2014-15.pdf 
+budget -f 437 -l 447 -n MinistryofHealth 2014-15.pdf 
+budget -o -f 437 -l 438 -n MinistryofHealth-2 2014-15.pdf 
+budget -f 448 -l 458 -n UgandaAIDSCommission 2014-15.pdf 
+budget -o -f 448 -l 449 -n UgandaAIDSCommission-2 2014-15.pdf 
+budget -f 459 -l 465 -n UgandaCancerInstitute 2014-15.pdf 
+budget -o -f 459 -l 460 -n UgandaCancerInstitute-2 2014-15.pdf 
+budget -f 466 -l 470 -n UgandaHeartInstitute 2014-15.pdf 
+budget -o -f 466 -l 467 -n UgandaHeartInstitute-2 2014-15.pdf 
+budget -f 471 -l 474 -n NationalMedicalStores 2014-15.pdf 
+budget -o -f 471 -l 472 -n NationalMedicalStores-2 2014-15.pdf 
+budget -f 475 -l 479 -n KampalaCityCouncilAuthority 2014-15.pdf 
+budget -o -f 475 -l 476 -n KampalaCityCouncilAuthority-2 2014-15.pdf 
+budget -f 480 -l 484 -n HealthServiceCommission 2014-15.pdf 
+budget -o -f 480 -l 481 -n HealthServiceCommission-2 2014-15.pdf 
+budget -f 485 -l 488 -n UgandaBloodTransfusionService 2014-15.pdf 
+budget -o -f 485 -l 486 -n UgandaBloodTransfusionService-2 2014-15.pdf 
+budget -f 489 -l 494 -n MulagoHospitalComplex 2014-15.pdf 
+budget -o -f 489 -l 490 -n MulagoHospitalComplex-2 2014-15.pdf 
+budget -f 495 -l 500 -n ButabikaHospital 2014-15.pdf 
+budget -o -f 495 -l 496 -n ButabikaHospital-2 2014-15.pdf 
+budget -f 501 -l 578 -n ReferralHospitals 2014-15.pdf 
+budget -o -f 501 -l 502 -n ReferralHospitals-2 2014-15.pdf 
+budget -f 579 -l 602 -n MinistryofWaterandEnvironment 2014-15.pdf 
+budget -o -f 579 -l 580 -n MinistryofWaterandEnvironment-2 2014-15.pdf 
+budget -f 603 -l 605 -n KampalaCityCouncilAuthority 2014-15.pdf 
+budget -o -f 603 -l 604 -n KampalaCityCouncilAuthority-2 2014-15.pdf 
+budget -f 606 -l 614 -n NationalEnvironmentManagementAuthority 2014-15.pdf 
+budget -o -f 606 -l 607 -n NationalEnvironmentManagementAuthority-2 2014-15.pdf 
+budget -f 615 -l 620 -n NationalForestryAuthority 2014-15.pdf 
+budget -o -f 615 -l 616 -n NationalForestryAuthority-2 2014-15.pdf 
+budget -f 621 -l 651 -n MinistryofGenderLabourandSocialDevelopment 2014-15.pdf 
+budget -o -f 621 -l 622 -n MinistryofGenderLabourandSocialDevelopment-2 2014-15.pdf 
+budget -f 652 -l 654 -n KampalaCityCouncilAuthority 2014-15.pdf 
+budget -o -f 652 -l 653 -n KampalaCityCouncilAuthority-2 2014-15.pdf 
+budget -f 655 -l 657 -n EqualOpportunitiesCommission 2014-15.pdf 
+budget -o -f 655 -l 656 -n EqualOpportunitiesCommission-2 2014-15.pdf 
+budget -f 658 -l 660 -n OfficeofthePresident 2014-15.pdf 
+budget -o -f 658 -l 659 -n OfficeofthePresident-2 2014-15.pdf 
+budget -f 661 -l 667 -n MinistryofDefence 2014-15.pdf 
+budget -o -f 661 -l 662 -n MinistryofDefence-2 2014-15.pdf 
+budget -f 668 -l 670 -n ExternalSecurityOrganisation 2014-15.pdf 
+budget -o -f 668 -l 669 -n ExternalSecurityOrganisation-2 2014-15.pdf 
+budget -f 671 -l 688 -n MinistryofJusticeandConstitutionalAffairs 2014-15.pdf 
+budget -o -f 671 -l 672 -n MinistryofJusticeandConstitutionalAffairs-2 2014-15.pdf 
+budget -f 689 -l 700 -n MinistryofInternalAffairs 2014-15.pdf 
+budget -o -f 689 -l 690 -n MinistryofInternalAffairs-2 2014-15.pdf 
+budget -f 701 -l 706 -n Judiciary 2014-15.pdf 
+budget -o -f 701 -l 702 -n Judiciary-2 2014-15.pdf 
+budget -f 707 -l 710 -n LawReformCommission 2014-15.pdf 
+budget -o -f 707 -l 708 -n LawReformCommission-2 2014-15.pdf 
+budget -f 711 -l 714 -n UgandaHumanRightsCommission 2014-15.pdf 
+budget -o -f 711 -l 712 -n UgandaHumanRightsCommission-2 2014-15.pdf 
+budget -f 715 -l 717 -n LawDevelopmentCentre 2014-15.pdf 
+budget -o -f 715 -l 716 -n LawDevelopmentCentre-2 2014-15.pdf 
+budget -f 718 -l NaN -n UgandaRegistrationServicesBureau 2014-15.pdf 
+budget -o -f 718 -l 719 -n UgandaRegistrationServicesBureau-2 2014-15.pdf 
+budget -f NaN -l 721 -n AnnualBudgetPerformanceReport 2014-15.pdf 
+budget -o -f NaN -l NaN -n AnnualBudgetPerformanceReport-2 2014-15.pdf 
+budget -f 722 -l 727 -n NationalCitizenshipandImmigrationControl 2014-15.pdf 
+budget -o -f 722 -l 723 -n NationalCitizenshipandImmigrationControl-2 2014-15.pdf 
+budget -f 728 -l 731 -n DirectorateofPublicProsecutions 2014-15.pdf 
+budget -o -f 728 -l 729 -n DirectorateofPublicProsecutions-2 2014-15.pdf 
+budget -f 732 -l 743 -n UgandaPoliceinclLDUs 2014-15.pdf 
+budget -o -f 732 -l 733 -n UgandaPoliceinclLDUs-2 2014-15.pdf 
+budget -f 744 -l 751 -n UgandaPrisons 2014-15.pdf 
+budget -o -f 744 -l 745 -n UgandaPrisons-2 2014-15.pdf 
+budget -f 752 -l 756 -n JudicialServiceCommission 2014-15.pdf 
+budget -o -f 752 -l 753 -n JudicialServiceCommission-2 2014-15.pdf 
+budget -f 757 -l 765 -n OfficeofthePrimeMinister 2014-15.pdf 
+budget -o -f 757 -l 758 -n OfficeofthePrimeMinister-2 2014-15.pdf 
+budget -f 766 -l 773 -n MinistryofPublicService 2014-15.pdf 
+budget -o -f 766 -l 767 -n MinistryofPublicService-2 2014-15.pdf 
+budget -f 774 -l 780 -n MinistryofLocalGovernment 2014-15.pdf 
+budget -o -f 774 -l 775 -n MinistryofLocalGovernment-2 2014-15.pdf 
+budget -f 781 -l 788 -n EastAfricanCommunity 2014-15.pdf 
+budget -o -f 781 -l 782 -n EastAfricanCommunity-2 2014-15.pdf 
+budget -f 789 -l 792 -n NationalPlanningAuthority 2014-15.pdf 
+budget -o -f 789 -l 790 -n NationalPlanningAuthority-2 2014-15.pdf 
+budget -f 793 -l 800 -n KampalaCityCouncilAuthority 2014-15.pdf 
+budget -o -f 793 -l 794 -n KampalaCityCouncilAuthority-2 2014-15.pdf 
+budget -f 801 -l 805 -n PublicServiceCommission 2014-15.pdf 
+budget -o -f 801 -l 802 -n PublicServiceCommission-2 2014-15.pdf 
+budget -f 806 -l 810 -n LocalGovernmentFinanceCommission 2014-15.pdf 
+budget -o -f 806 -l 807 -n LocalGovernmentFinanceCommission-2 2014-15.pdf 
+budget -f 811 -l 860 -n MinistryofFinancePlanningEconomicDevelopment 2014-15.pdf 
+budget -o -f 811 -l 812 -n MinistryofFinancePlanningEconomicDevelopment-2 2014-15.pdf 
+budget -f 861 -l 866 -n InspectorateofGovernment 2014-15.pdf 
+budget -o -f 861 -l 862 -n InspectorateofGovernment-2 2014-15.pdf 
+budget -f 867 -l 869 -n EthicsandIntegrity 2014-15.pdf 
+budget -o -f 867 -l 868 -n EthicsandIntegrity-2 2014-15.pdf 
+budget -f 870 -l 871 -n KampalaCityCouncilAuthority 2014-15.pdf 
+budget -o -f 870 -l 871 -n KampalaCityCouncilAuthority-2 2014-15.pdf 
+budget -f 872 -l 876 -n AuditorGeneral 2014-15.pdf 
+budget -o -f 872 -l 873 -n AuditorGeneral-2 2014-15.pdf 
+budget -f 877 -l 882 -n UgandaRevenueAuthority 2014-15.pdf 
+budget -o -f 877 -l 878 -n UgandaRevenueAuthority-2 2014-15.pdf 
+budget -f 883 -l 889 -n UgandaBureauofStatistics 2014-15.pdf 
+budget -o -f 883 -l 884 -n UgandaBureauofStatistics-2 2014-15.pdf 
+budget -f 890 -l 895 -n PublicProcurementandDisposalofAssets 2014-15.pdf 
+budget -o -f 890 -l 891 -n PublicProcurementandDisposalofAssets-2 2014-15.pdf 
+budget -f 896 -l 900 -n ParliamentaryCommission 2014-15.pdf 
+budget -o -f 896 -l 897 -n ParliamentaryCommission-2 2014-15.pdf 
+budget -f 901 -l 911 -n OfficeofthePresident 2014-15.pdf 
+budget -o -f 901 -l 902 -n OfficeofthePresident-2 2014-15.pdf 
+budget -f 912 -l 914 -n StateHouse 2014-15.pdf 
+budget -o -f 912 -l 913 -n StateHouse-2 2014-15.pdf 
+budget -f 915 -l 932 -n MinistryofForeignAffairs 2014-15.pdf 
+budget -o -f 915 -l 916 -n MinistryofForeignAffairs-2 2014-15.pdf 
+budget -f 933 -l 937 -n ElectoralCommission 2014-15.pdf 
+budget -o -f 933 -l 934 -n ElectoralCommission-2 2014-15.pdf 
