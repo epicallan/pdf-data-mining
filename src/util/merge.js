42e86e57144894d9a52c0f11b7bf84bd2040863b
allan <epicallan.al@gmail.com>
2016-05-29 23:04:33 +0300

    merging files working

--- src/util/merge.js
+++ src/util/merge.js
@@ -2,12 +2,12 @@ import fs from 'fs';
 import readline from 'readline';
 
 // const spawn = require('child_process').spawn;
-
+const buffers = new Map();
 const rootPath = '/Users/allanlukwago/apps/budget-data/samples';
 
 // returns a file stream, which the csv stream pipes into
 const writableStream = () => {
-  const stream = fs.createWriteStream((`${rootPath}/merged.csv`));
+  const stream = fs.createWriteStream((`${rootPath}/mergedNew.csv`));
   return stream;
 };
 
@@ -16,25 +16,45 @@ const readStream = (fileName) => (
     input: fs.createReadStream(`${rootPath}/${fileName}`)
   }));
 
-const tableHeading = (line) =>
+const findTableHeading = (line) =>
   (/(^\bVote Name\b)(.*\bTable Name\b)/.test(line));
 
 
-function mergeFiles(files, writerStream, reader) {
-  let isTableHeadingLine = false;
-  const writer = writerStream();
-  files.forEach((fileName, index) => {
-    const stream = reader(fileName);
+const createsReadStreams = (files) =>
+  (files.map((fileName) => readStream(fileName)));
+
+function fillDataBuffers(readerStreams) {
+  let isATableHeadingLine = false;
+  readerStreams.forEach((stream, index) => {
+    const streamData = [];
     stream.on('line', (line) => {
       // we want to record the first table heading column
-      if (index > 0) isTableHeadingLine = tableHeading(line);
-      if (!isTableHeadingLine) writer.write(`${line}\n`);
+      if (index > 0) isATableHeadingLine = findTableHeading(line);
+      if (!isATableHeadingLine) {
+        streamData.push(`${line}\n`);
+        buffers.set(index, streamData);
+      }
+    });
+  });
+}
+
+function writeBuffersToFile(readerStreams) {
+  const writer = writableStream();
+  readerStreams.forEach((stream, index) => {
+    stream.on('close', () => {
+      const buffer = buffers.get(index);
+      // cleanup buffers
+      buffers.delete(index);
+      // write to file
+      buffer.forEach(line => writer.write(line));
     });
   });
 }
 
 function main() {
-  mergeFiles(['test1.csv', 'test2.csv'], writableStream, readStream);
+  const streams = createsReadStreams(['test1.csv', 'test2.csv']);
+  fillDataBuffers(streams);
+  writeBuffersToFile(streams);
 }
 
 main();
