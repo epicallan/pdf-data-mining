39a538d860d97d2d2380c63e650b118e1ccb70c0
allan <epicallan.al@gmail.com>
2016-05-29 14:38:45 +0300

    taking care of 2011-2012 over view tables difference

--- src/config.js
+++ src/config.js
@@ -39,15 +39,32 @@ export const transformRegular = (row) => ({
   '% releases spent': row[6]
 });
 // overviewVoteExpenditure tables have different table names and structure
-export const transformOverview = (row) => ({
-  'Vote Name': row[9],
-  'Table Name': row[8],
-  Category: row[0],
-  'Approved Budget': row[1],
-  'Cashlimits by End': row[2],
-  'Released by End': row[3],
-  'Spent by End Jun': row[4],
-  '% Budget Released': row[5],
-  '% Budget spent': row[6],
-  '% Releases spent': row[7]
-});
+// 2011 -2012 overview tables dont have the Cashlimits column and hence have less fields
+const rowStructure = row => {
+  if (row.length === 10) {
+    return { // rest of the years
+      'Vote Name': row[9],
+      'Table Name': row[8],
+      Category: row[0],
+      'Approved Budget': row[1],
+      'Cashlimits by End': row[2],
+      'Released by End': row[3],
+      'Spent by End Jun': row[4],
+      '% Budget Released': row[5],
+      '% Budget spent': row[6],
+      '% Releases spent': row[7]
+    };
+  }
+  return { // 2011-12 tables
+    'Vote Name': row[8],
+    'Table Name': row[7],
+    Category: row[0],
+    'Approved Budget': row[1],
+    'Released by End': row[2],
+    'Spent by End Jun': row[3],
+    '% Budget Released': row[4],
+    '% Budget spent': row[5],
+    '% Releases spent': row[6]
+  };
+};
+export const transformOverview = (row) => rowStructure(row);
