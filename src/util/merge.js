f9ef35f3480390890d5c2c281b427201258772cb
allan <epicallan.al@gmail.com>
2016-06-02 01:07:48 +0300

    rx issue

--- src/util/merge.js
+++ src/util/merge.js
@@ -3,33 +3,33 @@ import readline from 'readline';
 import Rx from 'rxjs/Rx';
 import path from 'path';
 
-const buffers = new Map(); // for storing file data
-const rootPath = '/Users/allanlukwago/apps/budget-data/samples';
-// const rootPath = '/home/allan/budget-data-scraping/samples';
-
-
-// returns a write stream for writing to the merge file
-const writableStream = () => {
-  const stream = fs.createWriteStream((`${rootPath}/merged.csv`));
-  return stream;
-};
-
-const readStream = (filePath) => (
-  readline.createInterface({
-    input: fs.createReadStream(filePath)
+export const fromNodeStreamToObserverable = (stream, dataEventName, finishEventName) =>
+  (Rx.Observable.create(observer => {
+    stream.addListener(dataEventName, data => observer.next(data));
+    stream.addListener('error', error => observer.error(error));
+    stream.addListener(finishEventName, () => observer.complete());
+    stream.resume();
+    return () => {
+      stream.removeAllListener(dataEventName);
+    };
   }));
 
+// returns a write stream for writing to the merge file
+export const getWriter = (dir) => {
+  const mergedCsvFilePath = dir ?
+    path.resolve(dir, 'new1.csv') : path.resolve(__dirname, 'new.csv');
+  return fs.createWriteStream(mergedCsvFilePath);
+};
+
+export const readLineObs = (filePath) => {
+  const rl = readline.createInterface({ input: fs.createReadStream(filePath) });
+  return fromNodeStreamToObserverable(rl, 'line', 'close');
+};
 // we dont want to have all the table headings from various
 // tables in our final file, so when we find them
 // we will skip them save for the first one
-const findTableHeading = (line) =>
-  (/(^\bVote Name\b)(.*\bTable Name\b)/.test(line));
-
-
-const createsReadStreams = (files) =>
-  (files.map((fileName) => readStream(fileName)));
-
-export const hello = () => 'hello';
+// const findTableHeading = (line) =>
+//   (/(^\bVote Name\b)(.*\bTable Name\b)/.test(line));
 
 export const readDirFiles = (dir, annex, prefix) => {
   let re = new RegExp('.csv$');
@@ -43,38 +43,27 @@ export const readDirFiles = (dir, annex, prefix) => {
     .map(file => path.resolve(directory, file));
 };
 
-function fillDataBuffers(readerStreams) {
-  let isATableHeadingLine = false;
-  readerStreams.forEach((stream, index) => {
-    const streamData = [];
-    stream.on('line', (line) => {
-      // we want to record the first table heading row
-      if (index > 0) isATableHeadingLine = findTableHeading(line);
-      if (!isATableHeadingLine) {
-        streamData.push(`${line}\n`);
-        buffers.set(index, streamData);
-      }
-    });
-  });
-}
+export const ob = (file) =>
+  (Rx.Observable.create(observer => {
+    observer.next(file);
+  }));
 
-function writeBuffersToFile(readerStreams) {
-  const writer = writableStream();
-  readerStreams.forEach((stream, index) => {
-    stream.on('close', () => {
-      const buffer = buffers.get(index);
-      // cleanup buffers
-      buffers.delete(index);
-      // write each line to file
-      buffer.forEach(line => writer.write(line));
+export function writeToCsv(reader, writer) {
+  return Rx.Observable.create(observer => {
+    reader.on('line', (line) => {
+      // we want to record the first table heading row
+      observer.next(line);
+      writer.write(line);
     });
+    reader.on('error', (err) => observer.error(err));
+    reader.on('close', () => observer.complete('finished'));
   });
 }
 
 export default function main() {
-  const streams = createsReadStreams(['test1.csv', 'test2.csv']);
-  fillDataBuffers(streams);
-  writeBuffersToFile(streams);
+  // const streams = createsReadStreams(['test1.csv', 'test2.csv']);
+  // writeToCsv(streams);
+  // writeBuffersToFile(streams);
 }
 
 main();
--- src/util/tests/utils-test.js
+++ src/util/tests/utils-test.js
@@ -1,16 +1,52 @@
 import { expect } from 'chai';
-import { readDirFiles } from '../merge.js';
+import path from 'path';
+import { readDirFiles, readLineObs, getWriter, ob } from '../merge.js';
 
 describe('utils tests', () => {
   const rootPath = '/Users/allanlukwago/apps/budget-data/samples';
-  it('should return an Observable', (done) => {
+
+  it('should return a stream files', (done) => {
     const source = readDirFiles(rootPath);
     // const source2 = readDirFiles('-2');
     // const newSource = source.flatMap(files => Rx.Observable.from(files));
     source.subscribe(
       x => {
-        console.log('file is: ', x);
+        // console.log('file is: ', x);
         expect(x).to.be.a('string');
+      },
+      err => {
+        console.log(`Error ${err}`);
+      },
+      () => done()
+    );
+  });
+
+  it('should return lines from readline Observable and write to file', (done) => {
+    // const writer = getWriter(rootPath);
+    const file = path.resolve(rootPath, 'test1.csv');
+    readLineObs(file).subscribe(
+      lineToWrite => {
+        // writer.write(`${lineToWrite}\n`);
+        expect(lineToWrite).to.have.length.above(2);
+        expect(lineToWrite).to.be.a('string');
+      },
+      err => {
+        console.log(`Error ${err}`);
+      },
+      () => done()
+    );
+  });
+  it('should write lines from a stream of files to a csv file', (done) => {
+    // const writer = getWriter(rootPath);
+    const source = readDirFiles(rootPath)
+      .map(file => readLineObs(file));
+
+    source.subscribe(
+      line => {
+        console.log(line);
+        // writer.write();
+        expect(line).to.have.length.above(2);
+        expect(line).to.be.a('string');
       },
       err => {
         console.log(`Error ${err}`);
